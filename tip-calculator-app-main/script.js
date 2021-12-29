b1f6fdf761fcd5058f35070713e6e5d6ead5fbca
WatafacRumeinia <cosminflorinel23@gmail.com>
2021-12-29 21:13:11 +0200

    Sites

---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,14 @@
+class Calculator{
+
+}
+
+const inp=document.querySelector('input');
+const percent5=document.getElementById('5prc');
+const percent10=document.getElementById('10prc');
+const percent15=document.getElementById('15prc');
+const percent25=document.getElementById('25prc');
+const percent50=document.getElementById('50prc');
+const nrPpl=document.getElementById('nrOfPeople').value;
+let sum=document.getElementsByClassName('tip');
+
+sum.innerText="300";
